3e63200da3bb6c192e73f1feaf055ec76d3c9800
RIVEROS CLAUDIO ANTONIO <50504813+zubzertata@users.noreply.github.com>
2023-05-18 17:29:59 -0400

    Update categorias.js
    Limitar Caracteres

--- pages/categorias.js
+++ pages/categorias.js
@@ -1,11 +1,9 @@
 import { useState } from 'react';
 import { supabase } from '@/Lib/supabaseClient';
 import Link from 'next/link';
-import { Container, Row, Col, Button,Card } from 'react-bootstrap';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
-
-export async function getStaticProps() {
-   //esta sección del código realiza una consulta a la tabla "articulos" en la base de datos Supabase para obtener las categorías que tienen estado "true".
+export async function getServerSideProps() {
   const { data: articulos, error } = await supabase
     .from('articulo')
     .select('*')
@@ -13,7 +11,7 @@ export async function getStaticProps() {
   if (error) {
     return { props: { error: error.message } };
   }
-  //esta sección del código realiza una consulta a la tabla "categoria" en la base de datos Supabase para obtener las categorías que tienen estado "true".
+
   const { data: categorias } = await supabase
     .from('categoria')
     .select('*')
@@ -21,21 +19,38 @@ export async function getStaticProps() {
   if (error) {
     return { props: { error: error.message } };
   }
+
   return { props: { articulos, categorias } };
 }
-//esta sección del código define una función que agrega un producto al carrito de compras. Verifica si ya hay elementos en el carrito y luego agrega el producto seleccionado a la lista de productos existente.
+
+const truncateText = (text, maxLength) => {
+  if (text.length <= maxLength) {
+    return text;
+  }
+
+  const truncatedText = text.slice(0, maxLength);
+  const lastSpaceIndex = truncatedText.lastIndexOf(' ');
+
+  if (lastSpaceIndex === -1) {
+    return truncatedText + '...';
+  }
+
+  return truncatedText.slice(0, lastSpaceIndex) + '...';
+};
+
 const agregarAlCarrito = (producto) => {
   const carritoActual = JSON.parse(localStorage.getItem('carrito')) || [];
   const nuevoCarrito = [...carritoActual, producto];
   localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
 };
-// Define la página de inicio con un estado inicial para el filtro de categoría. Si hay un error, se muestra en pantalla.
+
 export default function HomePage({ articulos, categorias, error }) {
   const [filtroCategoria, setFiltroCategoria] = useState(null);
+
   if (error) {
     return <div>{error}</div>;
   }
- // Filtra los artículos según la categoría seleccionada o muestra todos si no hay filtro.
+
   const articulosFiltrados = articulos.filter((articulo) => {
     if (filtroCategoria === null) {
       return true;
@@ -45,52 +60,55 @@ export default function HomePage({ articulos, categorias, error }) {
 
   return (
     <Container className="my-5">
-            <Row className="mb-4 text-center">
+      <Row className="mb-4 text-center">
         <Col>
-            <h3>Filtrar por categoría:</h3>
-            <Button
+          <h3>Filtrar por categoría:</h3>
+          <Button
             variant={!filtroCategoria ? '' : ''}
             onClick={() => setFiltroCategoria(null)}
             className="me-2"
             style={{ backgroundColor: filtroCategoria === null ? '#D2CEF2' : '' }}
-            >
+          >
             Todos
-            </Button>
-            {categorias.map((categoria) => (
+          </Button>
+          {categorias.map((categoria) => (
             <Button
-                key={categoria.id_categoria}
-                variant={filtroCategoria === categoria.id_categoria ? '' : ''}
-                onClick={() => setFiltroCategoria(categoria.id_categoria)}
-                className="me-2"
-                style={{ backgroundColor: filtroCategoria === categoria.id_categoria ? '#D2CEF2' : '' }}
+              key={categoria.id_categoria}
+              variant={filtroCategoria === categoria.id_categoria ? '' : ''}
+              onClick={() => setFiltroCategoria(categoria.id_categoria)}
+              className="me-2"
+              style={{ backgroundColor: filtroCategoria === categoria.id_categoria ? '#D2CEF2' : '' }}
             >
-                {categoria.nombre}
+              {categoria.nombre}
             </Button>
-            ))}
+          ))}
         </Col>
-        </Row>
-        <Row xs={1} md={3} className="g-4">
-            {articulosFiltrados.map((articulo) => (
-            <Col key={articulo.id_articulo}>
-            <Card className='card h-100 d-flex flex-column'>
+      </Row>
+      <Row xs={1} md={3} className="g-4">
+        {articulosFiltrados.map((articulo) => (
+          <Col key={articulo.id_articulo}>
+            <Card className="card h-100 d-flex flex-column">
               <Card.Img variant="top" src={articulo.url_imagen} alt={articulo.nombre} />
               <Card.Body>
                 <Card.Title>{articulo.nombre}</Card.Title>
-                <Card.Text>{articulo.descripcion}</Card.Text>
+                <Card.Text>{truncateText(articulo.descripcion, 250)}</Card.Text>
                 <Card.Text>Precio: {articulo.precio_venta.toLocaleString('es-CL')}</Card.Text>
                 <Card.Text>Stock: {articulo.stock}</Card.Text>
                 <div className="d-flex justify-content-between">
-                  <button className="btn btn-primary" onClick={() => agregarAlCarrito(articulo)}>Agregar al carrito</button>
+                  <button className="btn btn-primary" onClick={() => agregarAlCarrito(articulo)}>
+                    Agregar al carrito
+                  </button>
                   <Link href={`./producto/${articulo.nombre.replaceAll(' ', '-')}`}>
-                    <button className="btn btn-secondary" style={{ backgroundColor: '#D2CEF2' }}>ver producto</button>
+                    <button className="btn btn-secondary" style={{ backgroundColor: '#D2CEF2' }}>
+                      Ver producto
+                    </button>
                   </Link>
                 </div>
               </Card.Body>
             </Card>
-            </Col>
-            ))}
-        </Row>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
-// 
